454e9bb013f4fd7de8736006e8556710c57c245c
Egor <egorgrot89136@gmail.com>
2023-11-25 16:45:47 +0300

    finalization of the form

--- src/component/ControlledInput.jsx
+++ src/component/ControlledInput.jsx
@@ -1,60 +1,94 @@
 import React, { useState } from "react";
 
 const ControlledInputs = ({ setUser, dataChangeable }) => {
-  const [firstname, setFirstname] = useState("");
-  const [lastNameInput, setLastname] = useState("");
-  const [date, setDate] = useState("");
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastNameInput: "",
+    date: ""
+  });
+  
+  const [formErrors, setFormErrors] = useState({
+    firstname: false,
+    lastNameInput: false,
+    date: false
+  });
 
-  const handleAddUserControlledInput = (e) => {
-    e.preventDefault();
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({...formData,[name]: value});
+    setFormErrors({...formErrors, [name]: false});
+  };
 
-    const name = firstname;
-    const lastName = lastNameInput;
+  const handleAddUserControlledInput = (event) => {
+    event.preventDefault();
+    const { firstname, lastNameInput, date } = formData;
     const dateOfBirth = new Date(date);
 
-    if (firstname !== "" && lastNameInput !== "" && date !== "") {
-      setUser({ id: String(dataChangeable.length + 1), name, lastName, dateOfBirth });
-      setFirstname("");
-      setLastname("");
-      setDate("");
+    if (firstname.trim() !== "" && lastNameInput.trim() !== "" && date !== "") {
+      setUser({
+        id: String(dataChangeable.length + 1),
+        name: firstname,
+        lastName: lastNameInput,
+        dateOfBirth,
+      });
+      setFormData({
+        firstname: "",
+        lastNameInput: "",
+        date: "",
+      });
+      setFormErrors({
+        firstname: false,
+        lastNameInput: false,
+        date: false
+      });
     } else {
-      alert("Заполните все поля!");
+      setFormErrors({
+        firstname: firstname.trim() === "",
+        lastNameInput: lastNameInput.trim() === "",
+        date: date === ""
+      });
     }
   };
 
   return (
     <div>
       <h1>Реализация через controlled inputs</h1>
-      <form>
-        <label>Имя</label>
+      <form onSubmit={handleAddUserControlledInput} noValidate>
+        <label htmlFor="firstname">Имя</label>
         <br />
         <input
           type="text"
+          name="firstname"
           required
-          value={firstname}
-          onChange={(e) => setFirstname(e.target.value)}
+          value={formData.firstname}
+          onChange={handleInputChange}
         />
+        {formErrors.firstname && <span style={{ color: "red" }}>Заполните поле "Имя"</span>}
         <br />
-        <label>Фамилия</label>
+        <label htmlFor="lastNameInput">Фамилия</label>
         <br />
         <input
           type="text"
+          name="lastNameInput"
           required
-          value={lastNameInput}
-          onChange={(e) => setLastname(e.target.value)}
+          value={formData.lastNameInput}
+          onChange={handleInputChange}
         />
+        {formErrors.lastNameInput && <span style={{ color: "red" }}>Заполните поле "Фамилия"</span>}
         <br />
-        <label>Дата рождения</label>
+        <label htmlFor="date">Дата рождения</label>
         <br />
         <input
           type="date"
+          name="date"
           style={{ width: "100px" }}
           required
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
+          value={formData.date}
+          onChange={handleInputChange}
         />
+        {formErrors.date && <span style={{ color: "red" }}>Заполните или выберите дату рождения</span>}
         <br />
-        <button onClick={handleAddUserControlledInput}>Add User</button>
+        <button  type="submit">Add User</button>
       </form>
     </div>
   );
