1336c2c5126e1976176cbfdb98683fc6f466c5cc
Egor <egorgrot89136@gmail.com>
2023-12-07 22:34:02 +0300

    technical improvements

--- src/App.js
+++ src/App.js
@@ -1,64 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useMemo, useCallback } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import ControlledInputs from "./component/ControlledInput";
 import SimpleInput from "./component/SimpleInput";
 
-const DeleteButton = ({ node }) => {
+const DeleteButton = ({ onDelete, node }) => {
   const handleDelete = () => {
-    console.log("id:", node.data.id);
+    onDelete(node.data.id);
   };
 
-  return (
-    <button onClick={handleDelete}>Удалить</button>
-  );
+  return <button onClick={handleDelete}>Удалить</button>;
 };
 
-function App() {
-  const data = [
-    { id: "1", name: "Egor", lastName: "Kuzmin", dateOfBirth: new Date("10-20-2000")},
-  ];
-  const [dataChangeable, setDataChangeable] = useState(data);
-
-  const lastNameComparator = (valueA, valueB) => {
-    let wordsA = valueA.split(" ");
-    let wordsB = valueB.split(" ");
-    const lastNameA = wordsA[1];
-    const lastNameB = wordsB[1];
-
-    return lastNameA > lastNameB ? 1 : -1;
-  };
-
+const formatDate = (date) => {
   const dateFormatter = new Intl.DateTimeFormat("ru", {
     day: "numeric",
     month: "long",
     year: "numeric",
   });
+  return dateFormatter.format(date);
+};
 
-  function formatDate(date) {
-    return dateFormatter.format(date);
-  }
-
-  const columnDefs = [
-    {
-      field: "name",
-      headerName: "Имя ",
-      valueGetter: (params) => `${params.data.name} ${params.data.lastName}`,
-      sortable: true,
-      comparator: lastNameComparator,
-    },
-    {
-      field: "dateOfBirth",
-      headerName: "Дата рождения",
-      valueGetter: (params) => formatDate(params.data.dateOfBirth),
-    },
-    { field: "delit", headerName: "Удалить", cellRenderer: DeleteButton },
-  ];
+const App = () => {
+  const [dataChangeable, setDataChangeable] = useState([]);
 
   const setUser = (user) => {
     setDataChangeable((prev) => [...prev, user]);
   };
+
+  const onDelete = useCallback((id) => {
+    // Handle delete logic here
+    console.log("Deleting item with id:", id);
+  }, []);
+
+  const columnDefs = useMemo(
+    () => [
+      {
+        field: "name",
+        headerName: "Имя",
+        valueGetter: (params) => `${params.data.name} ${params.data.lastName}`,
+        sortable: true,
+        comparator: (valueA, valueB) => {
+          let wordsA = valueA.split(" ");
+          let wordsB = valueB.split(" ");
+          const lastNameA = wordsA[1];
+          const lastNameB = wordsB[1];
+          return lastNameA > lastNameB ? 1 : -1;
+        },
+      },
+      {
+        field: "dateOfBirth",
+        headerName: "Дата рождения",
+        valueGetter: (params) => formatDate(params.data.dateOfBirth),
+      },
+      { 
+        field: "delete", 
+        headerName: "Удалить", 
+        cellRenderer: (props) => <DeleteButton onDelete={onDelete} {...props} /> 
+      },
+    ],
+    [onDelete]
+  );
 
   return (
     <div id="myGrid" className="ag-theme-alpine" style={{ height: "500px" }}>
@@ -67,6 +70,6 @@ function App() {
       <ControlledInputs setUser={setUser} dataChangeable={dataChangeable} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/component/ControlledInputs.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+const ControlledInputs = ({ setUser, dataChangeable }) => {
+  const [firstname, setFirstname] = useState('');
+  const [lastNameInput, setLastname] = useState('');
+  const [date, setDate] = useState('');
+
+  const addNewUserControlledInput = (e) => {
+    e.preventDefault();
+
+    const name = firstname;
+    const lastName = lastNameInput;
+    const dateOfBirth = new Date(date);
+
+    if (firstname !== '' && lastNameInput !== '' && date !== '') {
+      setUser({ id: String(dataChangeable.length + 1), name, lastName, dateOfBirth });
+      setFirstname('');
+      setLastname('');
+      setDate('');
+    } else {
+      alert('Заполните все поля!');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Реализация через controlled inputs</h1>
+      <form>
+        <label>First name:</label>
+        <br />
+        <input
+          type="text"
+          required
+          value={firstname}
+          onChange={(e) => setFirstname(e.target.value)}
+        />
+        <br />
+        <label>Last name:</label>
+        <br />
+        <input
+          type="text"
+          required
+          value={lastNameInput}
+          onChange={(e) => setLastname(e.target.value)}
+        />
+        <br />
+        <label>Date of birth:</label>
+        <br />
+        <input
+          type="date"
+          style={{ width: '100px' }}
+          required
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+        <br />
+        <button onClick={addNewUserControlledInput}>Add User</button>
+      </form>
+    </div>
+  );
+};
+
+export default ControlledInputs;
